af630c0534224e446fca03bab2214ae5a91670db
Gordon Chen <gordon890908@gmail.com>
2021-12-09 23:30:40 -0800

    Value can dynamically change

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -16,54 +16,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var rate: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        billAmountTextField.keyboardType = UIKeyboardType.numberPad
-        billAmountTextField.becomeFirstResponder()
-        billAmountTextField.clearButtonMode = .whileEditing
-        rate.keyboardType = UIKeyboardType.numberPad
-        // Do any additional setup after loading the view.
-    }
-    
     func roundToPlaces(_ value:Double, _ places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return round(value * divisor) / divisor
     }
     
-    @IBAction func sliderText(_ sender: Any) {
-        billAmountTextField.resignFirstResponder()
-        let bill = Double(billAmountTextField.text!) ?? 0
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        billAmountTextField.keyboardType = UIKeyboardType.numberPad
+        billAmountTextField.becomeFirstResponder()
+        billAmountTextField.clearButtonMode = .whileEditing
+        billAmountTextField.addTarget(self, action: #selector(textChanged), for: .editingChanged)
+        rate.keyboardType = UIKeyboardType.numberPad
+        rate.addTarget(self, action: #selector(textChanged), for: .editingChanged)
+        rate.clearButtonMode = .whileEditing
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func textChanged() {
         let percentage = Double(rate.text!) ?? 0 > 100 ? 100 : Double(rate.text!) ?? 0 < 0 ? 0 : Double(rate.text!) ?? 0
+        rate.text = String(format: "%.0f", percentage)
+        let bill = Double(billAmountTextField.text!) ?? 0
         let tip = bill * percentage / 100
         let total = bill + tip
-        rate.text = String(format: "%.0f", percentage)
+        slider.setValue(Float(percentage / 100), animated: true)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
     @IBAction func sliderTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
-        
         let percentage = Double(slider.value)
-        let tip = bill * percentage / 100
+        let tip = bill * percentage
         let total = bill + tip
-        rate.text = String(format: "%.0f", percentage)
+        rate.text = String(format: "%.0f", percentage * 100)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
-        
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill *
             tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
-        slider.setValue( Float(tipPercentages[tipControl.selectedSegmentIndex]) * 100, animated: true)
+        slider.setValue( Float(tipPercentages[tipControl.selectedSegmentIndex]), animated: true)
         rate.text = String(format: "%.0f", tipPercentages[tipControl.selectedSegmentIndex] * 100)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
 }
-
