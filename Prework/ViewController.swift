4b8a797fc10bbf4a1d6e81061727adef8e0d2e2d
Gordon Chen <gordon890908@gmail.com>
2021-12-10 00:29:43 -0800

    Add splitting function and fix slider bug

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var rate: UITextField!
+    @IBOutlet weak var splitControl: UISegmentedControl!
+    @IBOutlet weak var people: UILabel!
+    @IBOutlet weak var split: UILabel!
     
     func roundToPlaces(_ value:Double, _ places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
@@ -23,18 +26,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         billAmountTextField.keyboardType = UIKeyboardType.decimalPad
         billAmountTextField.becomeFirstResponder()
-        billAmountTextField.clearButtonMode = .whileEditing
         billAmountTextField.addTarget(self, action: #selector(textChanged), for: .editingChanged)
         rate.keyboardType = UIKeyboardType.numberPad
         rate.addTarget(self, action: #selector(textChanged), for: .editingChanged)
         rate.clearButtonMode = .whileEditing
-        // Do any additional setup after loading the view.
     }
     
-    // Dynamic change with any input change with textfield
+    // dynamic change with any input change with textfield
     @objc func textChanged() {
+        let count = [1,2,3,4,5,6]
         let percentage = Double(rate.text!) ?? 0 > 100 ? 100 : Double(rate.text!) ?? 0 < 0 ? 0 : Double(rate.text!) ?? 0
         rate.text = String(format: "%.0f", percentage)
         let bill = Double(billAmountTextField.text!) ?? 0
@@ -43,21 +46,25 @@ class ViewController: UIViewController {
         slider.setValue(Float(percentage / 100), animated: true)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        split.text = String(format: "$%.2f", total / Double(count[splitControl.selectedSegmentIndex]))
     }
     
-    // For slider
+    // for slider
     @IBAction func sliderTip(_ sender: Any) {
+        let count = [1,2,3,4,5,6]
         let bill = Double(billAmountTextField.text!) ?? 0
-        let percentage = Double(slider.value)
+        let percentage = Double(Int(slider.value * 100)) / 100
         let tip = bill * percentage
         let total = bill + tip
         rate.text = String(format: "%.0f", percentage * 100)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        split.text = String(format: "$%.2f", total / Double(count[splitControl.selectedSegmentIndex]))
     }
     
-    // For segment
+    // for rate segment
     @IBAction func calculateTip(_ sender: Any) {
+        let count = [1,2,3,4,5,6]
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill *
@@ -67,5 +74,18 @@ class ViewController: UIViewController {
         rate.text = String(format: "%.0f", tipPercentages[tipControl.selectedSegmentIndex] * 100)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        split.text = String(format: "$%.2f", total / Double(count[splitControl.selectedSegmentIndex]))
+    }
+    
+    // for splitting segment
+    @IBAction func splitTip(_ sender: Any) {
+        let count = [1,2,3,4,5,6]
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let percentage = Double(slider.value)
+        let tip = bill * percentage
+        let total = bill + tip
+        people.text = count[splitControl.selectedSegmentIndex] == 1
+                        ? "person" : "people"
+        split.text = String(format: "$%.2f", total / Double(count[splitControl.selectedSegmentIndex]))
     }
 }
